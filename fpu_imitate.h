e61301e183842f313484434c9e86d3b89a5d455f
MasayoshiTsutsui <tsutsui-masayoshi0528@g.ecc.u-tokyo.ac.jp>
2020-12-02 02:16:43 +0900

    all instructions completed!

---
+++ fpu_imitate.h
@@ -0,0 +1,10 @@
+#pragma once
+
+float myfmul(float, float);
+float myfdiv(float, float);
+float myfsqrt(float);
+int myftoi(float);
+float myitof(int);
+int myfless(float, float);
+int myfeq(float, float);
+float myfloor(float);
